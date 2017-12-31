70b0cf7af6ce2f6618e5a667c6388a954cb3cc93
Anthony McLin <anthony.mclin@rosetta.com>
2017-12-31 02:20:00 -0800

    notifications working with webpush

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const config = require('./config.json')
-//const webpush = require('web-push')
+const webPush = require('web-push')
 const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB()
 var registrations = []
 
+webPush.setVapidDetails(
+  'mailto:' + config.email,
+  config.publickey,
+  config.privatekey
+)
+
 /**
  * Sends a push notification to a specified endpoint
  **/
-function sendNotification(endpoint) {
-//   webPush.sendNotification({
-//     endpoint: endpoint
-//   }).then(function() {
-    console.log('Push Application Server - Notification sent to ' + JSON.stringify(endpoint));
-//   }).catch(function() {
-//     console.log('ERROR in sending Notification, endpoint removed ' + endpoint);
-//     unregisterSubscription(endpoint);
-//   });
+function sendNotification (subscription) {
+  const pushSubscription = subscription
+  // const pushSubscription = {
+  // endpoint: '< Push Subscription URL >',
+  //   keys: {
+  //     p256dh: '< User Public Encryption Key >',
+  //     auth: '< User Auth Secret >'
+  //   }
+  // }
+
+  const payload = config.message
+
+  webPush.sendNotification(
+    pushSubscription,
+    payload
+  ).then(function () {
+    console.log('Push Application Server - Notification sent to ' + subscription.endpoint)
+  }).catch(function (error) {
+    console.log('ERROR in sending Notification, endpoint removed ' + subscription.endpoint)
+    unregisterSubscription(subscription.endpoint)
+  })
 }
 
 /**
  * Remove a subscription registration from the server
  **/
-function unregisterSubscription(endpoint) {
+function unregisterSubscription (endpoint) {
   // TODO: add call to remove registration from dynamo
 }
 
@@ -29,35 +47,35 @@ function unregisterSubscription(endpoint) {
  * Filters the notification registrations down to those which
  * match the event
  **/
-function getRegistrations(locationid, callback) {
+function getRegistrations (locationid, callback) {
   var params = {
-    TableName : "laundry_notification_subscriptions",
-    ProjectionExpression:"#id, #keys",
-    FilterExpression: "locationid = :lid",
-    ExpressionAttributeNames:{
-        "#id": "endpoint",
-        "#keys": "keys"
+    TableName: 'laundry_notification_subscriptions',
+    ProjectionExpression: '#id, #keys',
+    FilterExpression: 'locationid = :lid',
+    ExpressionAttributeNames: {
+      '#id': 'endpoint',
+      '#keys': 'keys'
     },
     ExpressionAttributeValues: {
-      ":lid": {
-        "S": locationid
+      ':lid': {
+        'S': locationid
       }
     }
   }
 
-  dynamodb.scan(params, function(err, data) {
-    if(err) {
-      console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
-      throw "Unable to query DynamoDB"
+  dynamodb.scan(params, function (err, data) {
+    if (err) {
+      console.log('Unable to query. Error:', JSON.stringify(err, null, 2))
+      throw new Error('Unable to query DynamoDB')
     }
 
-    console.log("Found %s registered subscriptions for location %s", data.Items.length, locationid)
+    console.log('Found %s registered subscriptions for location %s', data.Items.length, locationid)
 
     // Convert from DynamoDB syntax into usable objects
-    registrations = data.Items.map(recordToJSON);
+    registrations = data.Items.map(recordToJSON)
 
     // Issue the callback to handle the results
-    if(typeof callback === 'function') { callback() }
+    if (typeof callback === 'function') { callback() }
   })
 }
 
@@ -79,10 +97,10 @@ function recordToJSON (record) {
 /**
  * Loop through list of registrations and send notifcations
  **/
-function processRegistrations() {
+function processRegistrations () {
   console.log('Processing %s registrations.', registrations.length)
-  registrations.forEach(function (el) {
-    sendNotification(el.endpoint)
+  registrations.forEach(function (subscription) {
+    sendNotification(subscription)
   })
 }
 
